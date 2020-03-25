239a094291ebd070b8f4ec7330b702843cff2cfb
assmannb <60766815+assmannb@users.noreply.github.com>
2020-03-25 13:41:52 -0700

    p7 done

--- p7_package/Generic_BST_Class.java
+++ p7_package/Generic_BST_Class.java
@@ -11,12 +11,21 @@ package p7_package;
  */
 public class Generic_BST_Class<GenericData extends Comparable<GenericData>>
 {
+    /**
+     * Binary Search Tree node class for managing generic data
+     *
+     */
     private class BST_Node
     {
         BST_Node leftChildRef;
         private GenericData nodeData;
         BST_Node rightChildRef;
 
+        /**
+         * Initialization constructor for data
+         * 
+         * @param inData - GenericData quantity
+         */
         public BST_Node( GenericData inData )
         {
             leftChildRef = null;
@@ -24,6 +33,13 @@ public class Generic_BST_Class<GenericData extends Comparable<GenericData>>
             nodeData = inData;
         }
 
+        /**
+         * Copy constructor for data
+         * <p>
+         * Note: Not used in class but available to user
+         * 
+         * @param copied - GenericData quantity
+         */
         public BST_Node( BST_Node copied )
         {
             leftChildRef = null;
@@ -35,78 +51,128 @@ public class Generic_BST_Class<GenericData extends Comparable<GenericData>>
     private BST_Node BST_Root;
     private GenericData removed;
 
+    /**
+     * Default class constructor, initializes BST
+     */
     Generic_BST_Class()
     {
         BST_Root = null;
     }
 
+    /**
+     * Clears tree
+     */
     public void clearTree()
     {
         BST_Root = null;
     }
 
+    /**
+     * Provides inOrder traversal action
+     * <p>
+     * Note: Calls displayInOrderHelper
+     */
     public void displayInOrder()
     {
         displayInOrderHelper( BST_Root );
     }
 
+    /**
+     * Provides inOrder traversal action using recursion
+     * 
+     * @param localRoot - BST_Node tree root reference at the current recursion
+     *                  level
+     */
     private void displayInOrderHelper( BST_Node localRoot )
     {
         System.out.println( "Data Display - Tree Display - Inorder:" );
-        if( localRoot.rightChildRef != null )
-        {
-            displayInOrderHelper( localRoot.rightChildRef );
-        }
-        if( localRoot.leftChildRef != null )
+        if( localRoot != null )
         {
             displayInOrderHelper( localRoot.leftChildRef );
+            System.out.println( localRoot.nodeData.toString() );
+            displayInOrderHelper( localRoot.rightChildRef );
         }
-        System.out.println( localRoot.nodeData.toString() );
     }
 
+    /**
+     * Provides postOrder traversal action
+     * <p>
+     * Note: Calls displayPostOrderHelper
+     */
     public void displayPostOrder()
     {
         displayPostOrderHelper( BST_Root );
     }
 
+    /**
+     * Provides postOrder traversal action using recursion
+     * 
+     * @param localRoot - BST_Node tree root reference at the current recursion
+     *                  level
+     */
     private void displayPostOrderHelper( BST_Node localRoot )
     {
         System.out.println( "Data Display - Tree Display - Postorder:" );
-        System.out.println( localRoot.nodeData.toString() );
-        if( localRoot.rightChildRef != null )
-        {
-            displayPostOrderHelper( localRoot.rightChildRef );
-        }
-        if( localRoot.leftChildRef != null )
+        if( localRoot != null )
         {
             displayPostOrderHelper( localRoot.leftChildRef );
+            displayPostOrderHelper( localRoot.rightChildRef );
+            System.out.println( localRoot.nodeData.toString() );
         }
     }
 
+    /**
+     * Provides preOrder traversal action
+     * <p>
+     * Note: Calls displayPreOrderHelper
+     */
     public void displayPreOrder()
     {
         displayPreOrderHelper( BST_Root );
     }
 
+    /**
+     * Provides preOrder traversal action using recursion
+     * 
+     * @param localRoot - BST_Node tree root reference at the current recursion
+     *                  level
+     */
     private void displayPreOrderHelper( BST_Node localRoot )
     {
-        System.out.println( "Data Display - Tree Display - Preorder:" );
-        if( localRoot.rightChildRef != null )
+        System.out.println( "Data Display - Tree Display - Postorder:" );
+        if( localRoot != null )
         {
+            System.out.println( localRoot.nodeData.toString() );
+            displayPreOrderHelper( localRoot.leftChildRef );
             displayPreOrderHelper( localRoot.rightChildRef );
         }
-        if( localRoot.leftChildRef != null )
-        {
-            displayPreOrderHelper( localRoot.leftChildRef );
-        }
-        System.out.println( localRoot.nodeData.toString() );
     }
 
+    /**
+     * Insert method for BST
+     * <p>
+     * Note: uses insert helper method which returns root reference
+     * 
+     * @param inData - GenericData data to be added to BST
+     */
     public void insert( GenericData inData )
     {
         BST_Root = insertHelper( BST_Root, inData );
     }
 
+    /**
+     * Insert helper method for BST insert action
+     * <p>
+     * Note: Recursive method returns updated local root to maintain tree
+     * linkage
+     * <p>
+     * Note: Must not allow duplicate keys (names)
+     * 
+     * @param localRoot - BST_Node tree root reference at the current recursion
+     *                  level
+     * @param inData    - GenericData item to be added to BST
+     * @return BST_Node reference used to maintain tree linkage
+     */
     private BST_Node insertHelper( BST_Node localRoot, GenericData inData )
     {
         if( localRoot == null )
@@ -126,11 +192,24 @@ public class Generic_BST_Class<GenericData extends Comparable<GenericData>>
         return localRoot;
     }
 
+    /**
+     * Test for empty tree
+     * 
+     * @return Boolean result of test
+     */
     public boolean isEmpty()
     {
         return BST_Root == null;
     }
 
+    /**
+     * Searches tree from given node to maximum value node below it, stores data
+     * value found, and then unlinks the node
+     * 
+     * @param maxParent - BST_Node reference to current node
+     * @param maxLoc    - BST_Node reference to child node to be tested
+     * @return BST_Node reference containing removed node
+     */
     private BST_Node removeFromMax( BST_Node maxParent, BST_Node maxLoc )
     {
         BST_Node maxTempNode;
@@ -143,14 +222,49 @@ public class Generic_BST_Class<GenericData extends Comparable<GenericData>>
         return maxTempNode;
     }
 
+    /**
+     * Removes data node from tree using given key
+     * <p>
+     * Note: uses remove helper method
+     * <p>
+     * Note: uses search initially to get value, if it is in tree; if value
+     * found, remove helper method is called, otherwise returns null
+     * 
+     * @param inData - GenericData that includes the necessary key
+     * @return GenericData result of remove action
+     */
     public GenericData removeItem( GenericData inData )
     {
         BST_Root = removeItemHelper( BST_Root, inData );
         return removed;
     }
 
+    /**
+     * Remove helper for BST remove action
+     * <p>
+     * Note: Recursive method returns updated local root to maintain tree
+     * linkage
+     * <p>
+     * Note: Sets member value removed to removed data for return by removeItem
+     * call
+     * <p>
+     * Note: uses removeFromMax method
+     * 
+     * @param localRoot - BST_Node tree root reference at the current recursion
+     *                  level
+     * @param outData   - GenericData item that includes the necessary key
+     * @return BST_Node reference result of remove helper action
+     */
     private BST_Node removeItemHelper( BST_Node localRoot, GenericData outData )
     {
+        if( localRoot.nodeData.compareTo( outData ) > 0 )
+        {
+            return removeItemHelper( localRoot.rightChildRef, outData );
+        }
+        else if( localRoot.nodeData.compareTo( outData ) < 0 )
+        {
+            return removeItemHelper( localRoot.leftChildRef, outData );
+        }
         if( localRoot.leftChildRef == null )
         {
             removed = localRoot.nodeData;
@@ -171,14 +285,28 @@ public class Generic_BST_Class<GenericData extends Comparable<GenericData>>
             localRoot = localRoot.leftChildRef;
             localRoot.leftChildRef = localRoot.leftChildRef.leftChildRef;
         }
-        return localRoot; // fix
+        return localRoot;
     }
 
+    /**
+     * Searches for data in BST given GenericData with necessary key
+     * 
+     * @param searchData - GenericData item containing key
+     * @return GenericData reference to found data
+     */
     public GenericData search( GenericData searchData )
     {
         return searchHelper( BST_Root, searchData );
     }
 
+    /**
+     * Helper method for BST search action
+     * 
+     * @param localRoot  - BST_Node tree root reference at the current recursion
+     *                   level
+     * @param searchData - GenericData item containing key
+     * @return GenericData item found
+     */
     private GenericData searchHelper( BST_Node localRoot,
             GenericData searchData )
     {
